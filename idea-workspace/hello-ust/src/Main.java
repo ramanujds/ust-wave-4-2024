9a82e14148421881c60154af2478ad94db629a3c
Ramanuj Das <ramanujds9@gmail.com>
2024-12-19 18:55:29 +0530

    updated Java Programs

--- idea-workspace/hello-ust/src/Main.java
+++ idea-workspace/hello-ust/src/Main.java
@@ -1,5 +1,28 @@
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        float value = 10.5f;
+        char ch = 'a';
+        boolean flag = true;
+
+        String name;
+
+        // Reading input from the user
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please input your name : ");
+        name = scan.nextLine();
+
+
+        System.out.println("Hello, I am " + name);
+
+//        System.out.println("""
+//                Hello, I am %s
+//                """.formatted(name));
+
+
     }
 }
---
+++ idea-workspace/hello-ust/src/maths/Calculator.java
@@ -0,0 +1,9 @@
+package maths;
+
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+
+}
